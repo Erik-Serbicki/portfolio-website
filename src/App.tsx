e719418467ec983d2be7a2ead1472cf4fb6185b1
Erik Serbicki <eriksertf@gmail.com>
2026-03-19 20:47:32 +0000

    created section layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import {Hero} from "@/sections/Hero.tsx" 
+import {About} from "@/sections/About.tsx" 
+import {Experience} from "@/sections/Experience.tsx" 
+import {Contact} from "@/sections/Contact.tsx" 
+import {Projects} from "@/sections/Projects.tsx" 
+import {Navbar} from "@/layout/Navbar.tsx" 
+
 function App() {
   return (
       <div className="min-h-screen overflow-x-hidden">
         <Navbar />
         <main>
+            <Hero />
+            <About />
+            <Experience />
+            <Projects />
+            <Contact />
         </main>
       </div>
   )
