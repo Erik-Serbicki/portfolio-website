69bdc53cb7a16438cebdf80896313a1b8eb90b6c
Erik Serbicki <eriksertf@gmail.com>
2026-03-31 17:40:23 +0000

    completed About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -8,7 +8,7 @@ const highlights = [
     },
     {
         icon: UsersRound,
-        title: "Teamwork",
+        title: "Managing",
         description: "I excel when working with others, and love sharing experiences and learning."
     },
     {
@@ -19,14 +19,14 @@ const highlights = [
     {
         icon: Atom,
         title: "Data Processing",
-        description: "I have worked on advanced data processing projects in Python, including cleaning, transformin, and analyzing large datasets from CERN."
+        description: "I have worked on advanced data processing projects in Python, including cleaning, transforming, and analyzing large datasets from CERN."
         
     },
 ]
 
 export const About = () => {
     return (
-        <section id="about" className="py-32 relative overflow-hidden">
+        <section id="about" className="py-24 relative overflow-hidden">
             <div className="container mx-auto px-6 relative z-10">
                 <div className="grid md:grid-cols-2 gap-16 items-center">
                     {/*Left Column*/}
@@ -38,18 +38,32 @@ export const About = () => {
                             Paving the road
                             <span className="font-serif italic font-normal text-white"> to a more connected world.</span>
                         </h2>
-                        <div>
+                        <div className="space-y-4 animate-fade-in animation-delay-200 text-muted-foreground">
                             <p>
-                                What does <span className="italics">connection</span> mean? To me, it has two different meanings: connecting ideas, and connecting with community. The former is what every strong 
+                                What does <span className="italic">connection</span> mean? To me, it has two different meanings: connecting ideas, and connecting with community. The former is what every strong 
                                 engineer needs to do, and the latter is the goal of all the projects I build and work I do. 
                             </p>
                             <p>
-                                I have 2+ years of experience as a Technical Project Manager, 3+ as a Software Engineer, 4+ as a Tennis Coach, a networking certification, a homelab setup, and a degree in Physics. I <span className="italics">connect</span> ideas from across these different disciplines.
-                                Phyiscs gives me problem solving, a curious mind, and a drive to learn (and no fewer than three notebooks filled with integrals). My love of homelabbing and my Network+ certification give me an intimate familiarity with bugs, issues, and things not working. 
+                                I have 2+ years of experience as a Technical Project Manager, 3+ as a Software Engineer, 4+ as a Tennis Coach, a networking certification, a homelab setup, and a degree in Physics. I <span className="italic">connect</span> ideas from across these different disciplines.
+                                Phyiscs gives me problem solving, a curious mind, and a drive to learn (and no fewer than three notebooks filled with integrals). 
+                            </p>
+                            <p>
+                                My love of homelabbing and my Network+ certification give me an intimate familiarity with bugs, issues, and things not working. 
                                 Managing gives me the desire to see projects to their completion, and an understanding of how to work with others to get there, regardless of their diverse backgrounds and outlooks. Coaching gives me patience, the ability to explain complex or technical concepts in simple terms,
-                                and is my primary <span className="italics">connection</span> to my community.
+                                and is my primary <span className="italic">connection</span> to my community.
                             </p>
                         </div>
+                    </div>
+
+                    {/*Right Column - Highlights*/}
+                    <div className="grid sm:grid-cols-2 gap-8">
+                        {highlights.map((highlight, index) => (
+                            <div className="glass rounded-2xl p-6 gap-4 animate-fade-in" key={index} style={{animationDelay: `${(index+1) * 100}ms`}}>
+                                <div className="h-12 w-12 rounded-xl bg-primary/10 flex items-center justify-center mb-4 text-primary hover:bg-primary/20 hover:text-white"><highlight.icon className="w-6 h-6"/></div>
+                                <h3 className="text-lg font-semibold mb-2">{highlight.title}</h3>
+                                <p className="text-muted-foreground text-sm">{highlight.description}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
